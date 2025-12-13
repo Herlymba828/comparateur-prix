b5efa55f633076a860ae391f4adaa1d1f35cded4
Herly MOULOUNGUI <herlymba828@gmail.com>
2025-12-13 23:03:38 +0100

    Désactiver middlewares personnalisés temporairement

--- config/settings.py
+++ config/settings.py
@@ -276,16 +276,16 @@ MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',  # Doit être placé avant CommonMiddleware
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'config.middleware.CompressionMiddleware',  # Compression optimisée (remplace GZipMiddleware)
-    'apps.api.compression_middleware.SmartCompressionMiddleware',  # Compression gzip intelligente
-    'apps.api.compression_middleware.JSONMinificationMiddleware',  # Minification JSON
+    # 'config.middleware.CompressionMiddleware',  # Désactivé temporairement
+    # 'apps.api.compression_middleware.SmartCompressionMiddleware',  # Désactivé temporairement
+    # 'apps.api.compression_middleware.JSONMinificationMiddleware',  # Désactivé temporairement
     'django.middleware.http.ConditionalGetMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'social_django.middleware.SocialAuthExceptionMiddleware',
     'django_otp.middleware.OTPMiddleware',
-    'apps.utilisateurs.middleware.AuthErrorEnhancementMiddleware',  # Améliore les réponses 401
-    'apps.api.middleware.PerformanceMonitoringMiddleware',  # Monitoring des performances
+    # 'apps.utilisateurs.middleware.AuthErrorEnhancementMiddleware',  # Désactivé temporairement
+    # 'apps.api.middleware.PerformanceMonitoringMiddleware',  # Désactivé temporairement
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'config.middleware.JSONExceptionMiddleware',  # Doit être en dernier pour intercepter toutes les exceptions
